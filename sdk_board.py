e214dca248ecff81edc5f4a632d4337921ef97dd
StarWarsSimba <miles.gurney@gmail.com>
2024-02-12 18:47:27 -0800

    Group

---
+++ sdk_board.py
@@ -0,0 +1,206 @@
+"""
+A Sudoku board holds a matrix of tiles.
+Each row and column and also sub-blocks
+are treated as a group (sometimes called
+a 'nonet'); when solved, each group must contain
+exactly one occurrence of each of the
+symbol choices.
+"""
+
+from sdk_config import CHOICES, UNKNOWN, ROOT
+from sdk_config import NROWS, NCOLS
+import enum
+from typing import Sequence, List, Set
+import logging
+logging.basicConfig()
+log = logging.getLogger(__name__)
+log.setLevel(logging.INFO)
+
+
+# --------------------------------
+#  The events for MVC
+# --------------------------------
+
+
+class Event(object):
+    """Abstract base class of all events, both for MVC
+    and for other purposes.
+    """
+    pass
+
+
+# ---------------
+# Listeners (base class)
+# ---------------
+
+
+class Listener(object):
+    """Abstract base class for listeners.
+    Subclass this to make the notification do
+    something useful.
+    """
+
+    def __init__(self):
+        """Default constructor for simple listeners without state"""
+        pass
+
+    def notify(self, event: Event):
+        """The 'notify' method of the base class must be
+        overridden in concrete classes.
+        """
+        raise NotImplementedError("You must override Listener.notify")
+
+
+# --------------------------------------
+# Events and listeners for Tile objects
+# --------------------------------------
+
+class EventKind(enum.Enum):
+    TileChanged = 1
+    TileGuessed = 2
+
+
+class TileEvent(Event):
+    """Abstract base class for things that happen
+    to tiles. We always indicate the tile.  Concrete
+    subclasses indicate the nature of the event.
+    """
+
+    def __init__(self, tile: 'Tile', kind: EventKind):
+        self.tile = tile
+        self.kind = kind
+        # Note 'Tile' type is a forward reference;
+        # Tile class is defined below
+
+    def __str__(self):
+        """Printed representation includes name of concrete subclass"""
+        return f"{repr(self.tile)}"
+
+
+class TileListener(Listener):
+    def notify(self, event: TileEvent):
+        raise NotImplementedError(
+            "TileListener subclass needs to override notify(TileEvent)")
+
+
+class Listenable:
+    """Objects to which listeners (like a view component) can be attached"""
+
+    def __init__(self):
+        self.listeners = [ ]
+
+    def add_listener(self, listener: Listener):
+        self.listeners.append(listener)
+
+    def notify_all(self, event: Event):
+        for listener in self.listeners:
+            listener.notify(event)
+
+
+# ----------------------------------------------
+#      Tile class
+# ----------------------------------------------
+
+
+class Tile(Listenable):
+    """One tile on the Sudoku grid.
+    Public attributes (read-only): value, which will be either
+    UNKNOWN or an element of CHOICES; candidates, which will
+    be a set drawn from CHOICES.  If value is an element of
+    CHOICES,then candidates will be the singleton containing
+    value.  If candidates is empty, then no tile value can
+    be consistent with other tile values in the grid.
+    value is a public read-only attribute; change it
+    only through the access method set_value or indirectly
+    through method remove_candidates.
+    """
+    def __init__(self, row: int, col: int, value=UNKNOWN):
+        super().__init__()
+        assert value == UNKNOWN or value in CHOICES
+        self.row = row
+        self.col = col
+        self.set_value(value)
+
+    def set_value(self, value: str):
+        if value in CHOICES:
+            self.value = value
+            self.candidates = {value}
+        else:
+            self.value = UNKNOWN
+            self.candidates = set(CHOICES)
+        self.notify_all(TileEvent(self, EventKind.TileChanged))
+
+    def __str__(self) -> str:
+        return f"{self.value}"
+
+    def __repr__(self) -> str:
+        return f"Tile({self.row}, {self.col}, '{self.value}')"
+
+    def could_be(self, value: str) -> bool:
+        """True iff value is a candidate value for this tile"""
+        return value in self.candidates
+
+    def __hash__(self) -> int:
+        """Hash on position only (not value)"""
+        return hash((self.row, self.col))
+
+
+# ------------------------------
+#  Board class
+# ------------------------------
+
+
+class Board(object):
+    """A board has a matrix of tiles"""
+
+    def __init__(self):
+        """The empty board"""
+        # Row/Column structure: Each row contains columns
+        self.tiles: List[List[Tile]] = []
+        self.groups = []
+        for row in self.tiles:
+            self.groups.append(row)
+        for row in range(NROWS):
+            column = []
+            for col in range(NCOLS):
+                column.append(self.tiles[row][col])
+            self.groups.append(column)
+        for block_row in range(ROOT):
+            for block_col in range(ROOT):
+                group = []
+                for row in range(ROOT):
+                    for col in range(ROOT):
+                        row_addr = (ROOT * block_row) + row
+                        col_addr = (ROOT * block_col) + col
+                        group.append(self.tiles[row_addr][col_addr])
+                self.groups.append(group)
+        for row in range(NROWS):
+            cols = []
+            for col in range(NCOLS):
+                cols.append(Tile(row, col))
+            self.tiles.append(cols)
+
+    def set_tiles(self, tile_values: Sequence[Sequence[str]]):
+        """Set the tile values a list of lists or a list of strings"""
+        for row_num in range(NROWS):
+            for col_num in range(NCOLS):
+                tile = self.tiles[row_num][col_num]
+                tile.set_value(tile_values[row_num][col_num])
+
+    def __str__(self) -> str:
+        """In Sadman Sudoku format"""
+        row_syms = []
+        for row in self.tiles:
+            values = [tile.value for tile in row]
+            row_syms.append("".join(values))
+        return "\n".join(row_syms)
+
+    def is_consistent(self) -> bool:
+        for row in range(len(self.tiles)):
+            used_symbols = set()
+            for col in range(row):
+                if col not in used_symbols:
+                    used_symbols.add(col)
+                else:
+                    return False
+        return True
